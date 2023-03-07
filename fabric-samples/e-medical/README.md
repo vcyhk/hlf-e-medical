c6578bf4dac97d580cf9b348fb3dadb42e84b925
v_cyhk <54260064+vcyhk@users.noreply.github.com>
2023-03-07 02:43:30 -0500

    init

---
+++ fabric-samples/e-medical/README.md
@@ -0,0 +1,29 @@
+# E-Medical
+## Wireframe
+![wireframe](wireframe.jpeg)
+
+## Requirement
+### Golang
+Install golang from [https://golang.org/dl/](https://golang.org/dl/)
+### node
+Install node from [https://nodejs.org/en/](https://nodejs.org/en/)
+### mongodb
+Install mongodb from [https://www.mongodb.com/download-center/community/](https://www.mongodb.com/download-center/community/)
+
+## Configuration
+Edit mongo db uri from main.go file. And install all npm packages for web app. Using command:
+```
+cd webapp
+npm install
+```
+
+## Running
+Run the following command to start the server.
+```
+go run main.go
+```
+
+Run the following command to start the webapp.
+```
+npm run start
+```
---
+++ fabric-samples/e-medical/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"e-madical/pkg/domain"
+	"e-madical/pkg/server"
+	"e-madical/pkg/service"
+	"e-madical/pkg/storage"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// mongodb uri
+	// example:
+	// mongodb+srv://<username>:<password>@<cluster-url>/<dbname>?retryWrites=true&w=majority
+	uri := "mongodb+srv://admin:admin@cluster0.e75lx8x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+	// create mongo client
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// connect to mongo database
+	err = client.Connect(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	// disconnect from mongo database after use
+	defer client.Disconnect(context.Background())
+
+	// get mongo database
+	db := client.Database("emedical")
+
+	var repo domain.Repository = storage.NewStorage(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	service := service.NewService(repo)
+
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	server := server.NewServer(r, service)
+
+	http.ListenAndServe(":8000", server)
+}
